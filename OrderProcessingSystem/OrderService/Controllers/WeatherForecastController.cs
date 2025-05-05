dc8d737b214ddb83297d0d576931b2d7583875d9
Errol <errolmcfadden@hotmail.com>
2025-05-05 23:21:37 +1000

    - Fixed docker port issue

--- OrderProcessingSystem/OrderService/Controllers/WeatherForecastController.cs
+++ OrderProcessingSystem/OrderService/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace OrderService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("getweather")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
